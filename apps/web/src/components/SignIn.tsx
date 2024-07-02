2b1bf2a57a8fb5384fc612f8f95a96cdb3c2edc7
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-07-02 20:37:14 +0530

    Fixes

--- apps/web/src/components/SignIn.tsx
+++ apps/web/src/components/SignIn.tsx
@@ -4,16 +4,16 @@ import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-import { Button } from "../../@/components/ui/button";
+import { Button } from "@repo/ui";
 import {
 	Card,
 	CardContent,
 	CardDescription,
 	CardHeader,
 	CardTitle,
-} from "../../@/components/ui/card";
-import { Input } from "../../@/components/ui/input";
-import { Label } from "../../@/components/ui/label";
+} from "@repo/ui";
+import { Input } from "@repo/ui";
+import { Label } from "@repo/ui";
 
 const Signin = () => {
 	const session = useSession();
