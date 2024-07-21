9373758a8e3dc9566f17d3570b0b787cb9438f51
iamnithishraja <153272518+iamnithishraja@users.noreply.github.com>
2024-07-21 20:17:06 +0530

    dummy

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,48 +1,49 @@
-"use server";
 import { SubjectType } from "@repo/db/client";
-import { getAllSubjectsByCollegeAndSem, getTracks } from "../actions/track";
-import prismaClient from "@repo/db/client";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../lib/auth";
+import { redirect } from "next/navigation";
+import { getCollege } from "../actions/college";
+import { getTracks, getAllSubjectsByCollegeAndSem } from "../actions/track";
 import { Appbar } from "../components/app-bar/AppBar";
 import { Subjects } from "../components/subjects";
 import { Tracks } from "../components/Tracks";
 import { TracksType } from "../types/userTypes";
-import { redirect } from "next/navigation";
-import Dropdown from "../components/Dropdown";
-import { getCollege } from "../actions/college";
 
 export default async function Page(): Promise<JSX.Element> {
-  const colleges: any = await getCollege();
-  const subjects: any = await getAllSubjectsByCollegeAndSem();
-  const tracks: any = await getTracks();
+	const tracks: any = await getTracks();
+	const subjects: any = await getAllSubjectsByCollegeAndSem();
+	const colleges: any = await getCollege();
 
-  if (subjects.error === "user not logged in") {
-    redirect("/auth");
-  }
+	if (subjects.error === "user not logged in") {
+		redirect("/auth");
+	}
 
-  return (
-    <div>
-      <Appbar />
-      <div className="flex justify-center pt-4">
-        <div className="text-zinc-950 dark:text-zinc-50 text-3xl p-2 max-w-screen-md font-semibold mt-2 mb-4">
-          Study Plans
-        </div>
-      </div>
-      {subjects.error || tracks.error ? (
-        <div className="grid gap-16">
-          <Dropdown colleges={colleges} />
-        </div>
-      ) : (
-        <div>
-          <Subjects
-            subjects={subjects.map((subject: SubjectType) => ({
-              subject: subject.name,
-            }))}
-          />
-          <Tracks tracks={tracks as unknown as TracksType[]} />
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<Appbar />
+			<div className="flex justify-center pt-4">
+				<div className="text-zinc-950 dark:text-zinc-50 text-3xl p-2 max-w-screen-md font-semibold mt-2 mb-4">
+					Study Plans
+				</div>
+			</div>
+			{subjects.error || tracks.error ? (
+				<div className="grid gap-16">
+					<select name="College" id="college">
+						<h1>{colleges.id}</h1>
+						<option value="">Select College</option>
+						{colleges.map((college: any) => (
+							<option value={college.id}>{college.name}</option>
+						))}
+					</select>
+				</div>
+			) : (
+				<div>
+					<Subjects
+						subjects={subjects.map((subject: SubjectType) => ({
+							subject: subject.name,
+						}))}
+					/>
+					<Tracks tracks={tracks as unknown as TracksType[]} />
+				</div>
+			)}
+		</div>
+	);
 }
